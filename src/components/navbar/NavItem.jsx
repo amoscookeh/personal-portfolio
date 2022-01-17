fb56511818a7173eb29f59bf6bb98c6a29ff6487
amoscookeh <amosliew1999@gmail.com>
2022-01-18 02:54:24 +0800

    invisible navbar fix

--- src/components/navbar/NavItem.jsx
+++ src/components/navbar/NavItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavItem = ({ index, closeSideMenu, item, showMenu }) => {
 
-    return (<div key={index} onClick={closeSideMenu} className={`mx-4 text-white hover:text-theme ${index === 0 ? "text-theme" : "text-white"} ${showMenu ? "" : "text-transparent"} py-3 md:py-0`}>
+    return (<div key={index} onClick={closeSideMenu} className={`md:text-base text-sm mx-4 text-white hover:text-theme ${index === 0 ? "text-theme" : "text-white"} py-3 md:py-0`}>
         <a href={item.itemRef} >
             {item.itemName}
         </a>

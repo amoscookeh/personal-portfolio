4e3d45c0a9435c647b014d943441aba265fecf98
amoscookeh <amosliew1999@gmail.com>
2022-07-29 17:55:19 +0800

    BeeX reflection css changes

--- src/components/blogs/BlogThumbnail.jsx
+++ src/components/blogs/BlogThumbnail.jsx
@@ -5,14 +5,14 @@ const BlogThumbnail = ({ name, component, thumbnail, date, synopsis, idx }) => {
   return (
     <div class="card w-96 bg-base-100 shadow-xl image-full">
       <figure>
-        <img src={thumbnail} alt="Thumbnail" />
+        <img className="bg-dark-body" src={thumbnail} alt="Thumbnail" />
       </figure>
       <div class="card-body">
         <h2 class="card-title text-theme">{name}</h2>
         <p class="text-white">{synopsis}</p>
         <div class="card-actions justify-end">
           <Link class="btn btn-primary text-white" to={`/blog/${idx}`}>
-            View Now
+            View
           </Link>
         </div>
       </div>
